d033105225f21b05a29516de97e172e524703008
guisantosalves <workguisantos@gmail.com>
2022-04-26 17:22:12 -0400

    inserting axios and doing the post

--- src/components/login/IndexLogin.js
+++ src/components/login/IndexLogin.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form, Button, Container } from "react-bootstrap";
+import axios from "axios";
+
 
 function Login() {
+  const [post, setPost] = useState(null);
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const createPost = () => {
+    axios.post("http://localhost:3001/api/v1/postlogin", {
+      usuario: login,
+      senha: senha
+    }).then((response)=>{
+      setPost(response.data)
+    })
+    console.log(post)
+  }
+
+  const getLoginValue = (event)=>{
+    const loginValue = event.target.value;
+    setLogin(loginValue)
+  }
+
+  const getSenhaValue = (event) => {
+    const senhaValue = event.target.value;
+    setSenha(senhaValue) 
+  }
+
   return (
     <Container>
       <div style={style.container}>
@@ -9,15 +35,15 @@ function Login() {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicLogin">
               <Form.Label>Login de acesso</Form.Label>
-              <Form.Control placeholder="insira o login" />
+              <Form.Control placeholder="insira o login" onChange={getLoginValue}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" placeholder="insira a senha" />
+              <Form.Control type="password" placeholder="insira a senha" onChange={getSenhaValue}/>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" onClick={createPost}>
               Logar
             </Button>
           </Form>
@@ -32,7 +58,7 @@ const style = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: "35%",
+    marginTop: "25%",
   },
   box: {
     width: "400px",
